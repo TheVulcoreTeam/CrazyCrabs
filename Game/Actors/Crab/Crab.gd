125363ce6e5918268d6a6e4dc1cea8f664d87efc
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2020-07-11 17:48:55 -0400

    Ajustes al escape del cangrejo

--- Game/Actors/Crab/Crab.gd
+++ Game/Actors/Crab/Crab.gd
@@ -16,16 +16,16 @@ func _on_ExitTime_timeout():
 		$Collision.disabled = false
 		can_move = true
 		
-		var direction = Vector2.ONE.rotated(deg2rad(rand_range(0, 360))) * 10
+		var direction = Vector2(1, 0).rotated(deg2rad(rand_range(0, 360))) * 10
 		var tween = Tween.new()
 		tween.interpolate_property(
 			self,
-			"global_position",
-			global_position,
+			"position",
+			Vector2(384/2, 208/2),
 			global_position + direction,
 			4.5,
 			Tween.TRANS_LINEAR,
-			Tween.EASE_IN
+			Tween.EASE_IN_OUT
 		)
 		tween.start()
 		$Anim.play("Escape")
