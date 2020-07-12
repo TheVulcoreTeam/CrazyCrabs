dcabb4ed52a329f74c9ed4d27c4d897292d64b87
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2020-07-12 00:19:06 -0400

    Ahora los cangrejos si escapan

--- Game/Actors/Crab/Crab.gd
+++ Game/Actors/Crab/Crab.gd
@@ -2,29 +2,41 @@ extends GCrab
 
 class_name Crab
 
+var direction = Vector2(1, 0).rotated(deg2rad(rand_range(0, 360))) * 100
+var dest = Vector2.ZERO
+var c_t = 0
+var escape := false
+
 func capture():
 	.capture()
 	$Anim.play("Captured")
 	
+func _process(delta):
+	if c_t < 1 and escape:
+		c_t += delta * 1.4
+		global_position = Vector2(384/2, 208/2).linear_interpolate(dest, c_t)
+	else:
+		escape = false
 
 func _on_ExitTime_timeout():
 	if pot and pot.cover_off:
 		is_capture = false
 		$Collision.disabled = false
 		clickeable = true
+		escape = true
 		
-		var direction = Vector2(1, 0).rotated(deg2rad(rand_range(0, 360))) * 10
-		var tween = Tween.new()
-		tween.interpolate_property(
-			self,
-			"position",
-			Vector2(384/2, 208/2),
-			global_position + direction,
-			4.5,
-			Tween.TRANS_LINEAR,
-			Tween.EASE_IN_OUT
-		)
-		tween.start()
+		dest = direction + Vector2(384/2, 208/2)
+#		var tween = Tween.new()
+#		tween.interpolate_property(
+#			self,
+#			"position",
+#			Vector2(384/2, 208/2),
+#			Vector2(384/2, 208/2) + direction,
+#			0.5,
+#			Tween.TRANS_LINEAR,
+#			Tween.EASE_IN_OUT
+#		)
+#		tween.start()
 		$Anim.play("Escape")
 		Main.store_crab_cooking_amount -= 1
 		
